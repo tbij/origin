a53ea136eec035bc8b05601d5ba97564594d6910
Max Harlow <maxharlow@tbij.com>
2015-08-06 16:02:02 +0100

    Better JWT library

--- User.scala
+++ User.scala
@@ -1,14 +1,15 @@
 import scala.util.Try
+import scala.collection.JavaConversions._
 import rapture.json.Json
 import rapture.json.jsonBackends.argonaut._
-import pdi.jwt.{Jwt, JwtAlgorithm, JwtClaim}
+import com.auth0.jwt.{JWTSigner, Algorithm, JWTVerifier}
 
 case class User(name: String, email: String) {
 
   def token: String = {
-    val content = Json(this).toString
-    val claims = JwtClaim(content = content).issuedNow.expiresIn(60 * 60 * 24 * 7) // expires in a week
-    Jwt.encode(Json(claims).toString, Config.auth.key, JwtAlgorithm.HS256)
+    val claims = Map("content" -> Json(this).toString)
+    val options = new JWTSigner.Options().setAlgorithm(Algorithm.HS256).setIssuedAt(true).setExpirySeconds(60 * 60 * 24 * 7) // expires in a week
+    new JWTSigner(Config.auth.key).sign(claims, options)
   }
 
 }
@@ -17,9 +18,8 @@ object User {
 
   def fromToken(token: String): Option[User] = {
     val attempt = Try {
-      val decoded = Jwt.decode(token, Config.auth.key, Seq(JwtAlgorithm.HS256)).get
-      val claims = Json.parse(decoded).as[JwtClaim]
-      Json.parse(claims.content).as[User]
+      val decoded = new JWTVerifier(Config.auth.key).verify(token)
+      Json.parse(decoded("content").toString).as[User]
     }
     attempt.toOption
   }
